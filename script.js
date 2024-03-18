51702cb68df38f395b654afa2716067aff9e7104
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-03-18 18:48:13 +0100

    I fixed Some problems in functions!!!

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 // check for player
 let IsPlayer1=true;
 // Function of rull
-let roll=document.querySelector(".roll");
-roll.addEventListener('click',function(){
+let rolle = function(){
     let Current=document.querySelector(".player-active .current p");
     let Score=document.querySelector(".player-active p:first-of-type");
     let player1=document.querySelector(".player1");
@@ -19,7 +18,7 @@ roll.addEventListener('click',function(){
         }
         else{
             Current.textContent=0
-            if(IsPlayer1===true){
+            if(IsPlayer1){
                 player1.classList.remove("player-active")
                 player2.classList.add("player-active")
                 IsPlayer1=false
@@ -32,15 +31,37 @@ roll.addEventListener('click',function(){
         }
     
 
-});
+}
+let roll=document.querySelector(".roll");
+roll.addEventListener("click",rolle);
 // Function of hold
-let btnhold=document.querySelector(".hold").addEventListener("click",hold)
+let btnhold=document.querySelector(".hold");
 function hold(){
     let Current=document.querySelector(".player-active .current p");
     let Score=document.querySelector(".player-active p:first-of-type");
     let player1=document.querySelector(".player1");
     let player2=document.querySelector(".player2");
     Score.textContent=parseInt(Score.textContent)+parseInt(Current.textContent);
-    Current.textContent=0;
+        Current.textContent=0;
+    if(parseInt(Score.textContent)<100){
+        if(IsPlayer1){
+            player1.classList.remove("player-active")
+            player2.classList.add("player-active")
+            IsPlayer1=false
+        }
+        else{
+            player2.classList.remove("player-active")
+            player1.classList.add("player-active")
+            IsPlayer1=true
+        }
+    }
+    else{
+        roll.removeEventListener("click",rolle);
+        btnhold.removeEventListener("click",hold)
+        document.querySelector("section.player-active").style.backgroundColor="#5dff5d"
+        
+    }
+    
 
 }
+btnhold.addEventListener("click",hold);
